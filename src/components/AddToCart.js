bd5ac3d1e3ab8ac8afcb364bbecdf8478a7f5feb
Shailendra <shailendra.c@shyenatechyarn>
2023-02-11 23:21:25 +0530

    project complete

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -60,7 +60,7 @@ const AddToCart = ({ product }) => {
         />
         <Link
           to="/cart"
-          className="btn"
+          className="btn add-to-cart"
           onClick={() => addToCart(id, mainColor, amount, product)}
         >
           add to cart
@@ -113,6 +113,9 @@ const Wrapper = styled.section`
   .btn {
     margin-top: 1rem;
     width: 140px;
+  }
+  .add-to-cart {
+    text-align: center;
   }
 `;
 export default AddToCart;
